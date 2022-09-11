b66348eefc94f521b866707874ddbe20b8ba2de5
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-11 16:37:45 +0200

    Update update_delete.sql

--- update_delete.sql
+++ update_delete.sql
@@ -1,1 +1,5 @@
 UPDATE Customers SET CustomerName = "Mek Elek", City = "Aprajafalva" WHERE CustomerID = 1;
+
+UPDATE Customers SET CustomerName = "Rejtő jenő", City = "Szőke ciklon" WHERE PostalCode = "S-958 22";
+
+
