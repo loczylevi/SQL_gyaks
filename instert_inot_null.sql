8cb0386e92b3c6e9f81270843a55caa6bd1f6672
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-11 15:53:14 +0200

    Create instert_inot_null.sql

---
+++ instert_inot_null.sql
@@ -0,0 +1,6 @@
+/*1. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Categories' táblájában
+a, beszúr egy rekordot, amelyben CategoryName és a Description értékek is meg vannak adva (CaregoryID automatikusan jön létre),
+b, beszúr egy rekordot, amelbyen csak a CategoryName értéke van megadva (CaregoryID automatikusan jön létre),
+c, lekérdezi azokat a rekordokat (az összes adatával), ahol a Description értéke NULL!*/
+
