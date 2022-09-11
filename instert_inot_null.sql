e963656c85f84e864215cdb33176be2afab791b1
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-11 16:07:31 +0200

    Update instert_inot_null.sql

--- instert_inot_null.sql
+++ instert_inot_null.sql
@@ -1,6 +1,20 @@
 /*1. Feladat
 Írj SQL utasítást amely az online tesztadatbázis 'Categories' táblájában
-a, beszúr egy rekordot, amelyben CategoryName és a Description értékek is meg vannak adva (CaregoryID automatikusan jön létre),
-b, beszúr egy rekordot, amelbyen csak a CategoryName értéke van megadva (CaregoryID automatikusan jön létre),
+a, beszúr egy rekordot, amelyben CategoryName és a Description értékek is meg vannak adva (CaregoryID automatikusan jön létre),*/
+
+
+INSERT INTO Categories (CategoryName,Description)
+VALUES ("Cuius regio eius religio","Akié a föld azé vallás")
+
+/*
+b, beszúr egy rekordot, amelbyen csak a CategoryName értéke van megadva (CaregoryID automatikusan jön létre),*/
+
+
+INSERT INTO Categories (CategoryName)
+VALUES ("Divida et impara")           -- Oszd meg és uralkodj
+
+/*
 c, lekérdezi azokat a rekordokat (az összes adatával), ahol a Description értéke NULL!*/
 
+
+SELECT * FROM Categories WHERE Description IS NULL
